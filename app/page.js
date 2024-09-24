2551194159b97ac005d41945844b68f6aa9bce41
muchomo <tastelikecrypto@gmail.com>
2024-09-24 15:20:00 -0600

    fixed styling errors

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from '../app/page.module.css'
 
 const HomePage = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.companyName}>Denomino.Digital</h1>
+    <div className={styles.container}>
+      <h1 className={styles.companyName}>Denomino.Digital</h1>
 
-      <section style={styles.section}>
+      <section className={styles.section}>
         <h2>About Our Brands</h2>
         <p>
           We provide digital menus and IT solutions tailored to various retail business types.
@@ -14,34 +15,14 @@ const HomePage = () => {
         </p>
       </section>
 
-      <section style={styles.section}>
+      <section className={styles.section}>
         <h2>Contact Us</h2>
         <p>
-          Have questions? Reach us at: <strong>(505) 600-0885</strong>
+          Have questions? Reach us at: <strong>505-600-0885</strong>
         </p>
       </section>
     </div>
   );
-};
-
-const styles = {
-  container: {
-    fontFamily: 'Arial, sans-serif',
-    textAlign: 'center',
-    padding: '20px',
-  },
-  companyName: {
-    fontSize: '2.5rem',
-    height: '100px',
-    margin: '20px 0',
-  },
-  section: {
-    width: '60%',
-    height:'200px',
-    margin: 'auto',
-    display:'block',
-    
-  },
 };
 
 export default HomePage;
